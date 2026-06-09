9a5979e7c1141eecbabd02c3774dbbcd7e8f8602
ithouseacademy <ithouselevelup@gmail.com>
2026-06-09 17:02:52 +0500

    first commit 1.0.13

--- groups/certificate_utils.py
+++ groups/certificate_utils.py
@@ -66,8 +66,8 @@ def generate_certificate_image(student_name, group_name, teacher_name, level, sc
     student_name = student_name.title()
 
     name_font = _get_font('Parisienne-Regular.ttf', int(img_w * 0.068))
-    level_font = _get_font('arial.ttf', int(img_w * 0.022))
-    teacher_font = _get_font('arial.ttf', int(img_w * 0.016))
+    level_font = _get_font('arial.ttf', 14)
+    teacher_font = _get_font('arial.ttf', 16)
 
     draw.text(
         (img_w / 2, img_h * 0.52),
